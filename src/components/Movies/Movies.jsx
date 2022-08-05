96256f8c2da8b2e242ebfea3cd88b339aff78179
bebo698 <attasalah34@gmail.com>
2022-08-05 09:37:51 +0200

    Actullay  Section handled more (seris- movies )

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,53 +1,89 @@
 import React, { Component } from 'react';
 import axios from "axios";
-export default class Movies extends Component {
 
-    state = { Movies: [] }
-    gettrending = async () => {
-        let { data } = await axios.get(`https://api.themoviedb.org/3/trending/tv/day?api_key=52bbcddeda849047525b51d6f8a12361`)
+export default class Movies extends Component {
+    state = { movie: [], tv: [] }
+    gettrending = async (mediatype) => {
+        let { data } = await axios.get(`https://api.themoviedb.org/3/trending/${mediatype}/day?api_key=52bbcddeda849047525b51d6f8a12361`);
+
         console.log(data.results);
-        this.setState({ Movies: data.results })
+        this.setState({ [mediatype]: data.results })
     }
     componentDidMount() {
-        this.gettrending();
+        this.gettrending('movie');
+        this.gettrending('tv');
+
     };
     render() {
         return (
             <>
-                <div className="className my-5">
-                     
-                        <div className="row   flex-row-reverse">
+                <div className='d-flex justify-content-center my-9'>مرحبا بك فى موقع هابي تايم </div>
+                <div className="container my-5 ">
 
-                            <div className="col-md-4 d-flex flex-column justify-content-center align-items-center bg-danger">
-                                <h2>     rending now </h2>
-                                <p>most watche</p>
-                            </div>
 
-                            {
-                                this.state.Movies.map((value, index) => {
-                                    return (
-                                        <>
+                    <div className="row   flex-row-reverse">
 
-                                            <div key={index} className="col-md-2 my-3">
-                                                <div className='item'>
-                                                    
-                                                    <img className='w-100' src={"https://image.tmdb.org/t/p/original/" + value.poster_path} />
-                                                    <h1>{value.name} {value.title}</h1>
-                                                    <span className='rate'> {value.vote_average}</span>
+                        <div className="col-md-4 d-flex flex-column justify-content-center align-items-center bg-danger">
+                            <h2>Trending Movies now </h2>
+                            <p>Most watched </p>
+                        </div>
 
-                                                </div>
+                        {
+                            this.state.movie.slice(0, 10).map((value, index) => {
+                                return (
+                                    <>
+
+                                        <div key={index} className="container col-md-2   my-5">
+                                            <div className='item'>
+
+                                                <img className='w-100' src={"https://image.tmdb.org/t/p/original/" + value.poster_path} />
+                                                <h3>{value.name} {value.title}</h3>
+                                                <span className='rate'> {value.vote_average}</span>
 
                                             </div>
-                                        </>
-                                    )
+
+                                        </div>
+                                    </>
+                                )
 
 
-                                })
+                            })
 
-                            }
+                        }
 
+                    </div>
+                    <div className="row   flex-row-reverse">
+
+                        <div className="col-md-4 d-flex flex-column justify-content-center align-items-center bg-danger">
+                            <h2>Trending Seris now </h2>
+                            <p>Most watched </p>
                         </div>
-                    
+
+                        {
+                            this.state.tv.slice(0, 10).map((value, index) => {
+                                return (
+                                    <>
+
+                                        <div key={index} className="container col-md-2   my-5">
+                                            <div className='item'>
+
+                                                <img className='w-100' src={"https://image.tmdb.org/t/p/original/" + value.poster_path} />
+                                                <h3>{value.name} {value.title}</h3>
+                                                <span className='rate'> {value.vote_average}</span>
+
+                                            </div>
+
+                                        </div>
+                                    </>
+                                )
+
+
+                            })
+
+                        }
+
+                    </div>
+
                 </div>
             </>
         )
