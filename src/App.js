6a19587e60e1e3803e194dc76cd9014a6bcfaa43
bebo698 <attasalah34@gmail.com>
2022-08-26 16:36:03 +0200

    changing in Navbar and Structure with scss

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 import React, { Component } from 'react';
-import Home from './components/Home/Home.jsx';
-import Movies from './components/Movies/Movies.jsx';
-import Navbar from './components/Navbar/Navbar.jsx';
-import Tv from './components/Tv/Tv';
-import Notfound from './components/Notfound/Notfound.jsx'
+// import _index from './scss/_index.scss';
+import './App.scss';
+// import $ from 'jquery';
+// import { findDOMNode } from 'react-dom';
+ 
+// import Movies from './components/Movies/Movies.jsx';
+// import Navbar from './components/Navbar/Navbar.jsx';
+// import Tv from './components/Tv/Tv';
+// import Notfound from './components/Notfound/Notfound.jsx'
 import Login from './components/Login/Login.jsx';
-import { Redirect, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
 import Register from './components/Register/Register.jsx';
-import Protectedroutes from './components/protectedroutes/protectedroutes.jsx';
-import 'bootstrap/dist/js/bootstrap'
- class App extends Component {
+// import Protectedroutes from './components/protectedroutes/protectedroutes.jsx';
+// import 'bootstrap/dist/js/bootstrap';
+// import './App.scss'
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/swiper.min.css';
+import Header from './components/header/Header.jsx';
+import Footer from './components/footer/Footer.jsx';
+import CRoutes from './config/CRoutes.jsx';
+// import { Autoplay, Pagination, Navigation } from "swiper";
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+class App extends Component {
   render() {
     return (
       <>
-        <Navbar />
+        {/* <Navbar /> */}
         {/* <ProtectedRoutes /> */}
-        <Routes>
-          {/* <Route path='/home'  element={<Home />}  /> */}
-          {/* <Protectedroutes  path="/home" element={Home} /> */}
+        {/* <Routes> */}
+        {/* <Route path='/home'  element={<Home />}  /> */}
+        {/* <Protectedroutes  path="/home" element={Home} /> */}
 
-          <Route  path="/home" element={<Protectedroutes />} />
+        {/* <Route  path="/home" element={<Protectedroutes />} /> */}
 
-          <Route path='/movie' element={<Movies/>} />
+        {/* <Route path='/movie' element={<Movies/>} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login  />} />
           <Route path='/tv' element={<Tv />} />
           <Route path='*' element={<Notfound/>}/>
 
-
+      
 
         </Routes>
-
-
-
+ */}
+        <Header />
+{/* 
+        <Routes>
+          <Route path='/*' element={<CRoutes />} />
+          
+        </Routes> */}
+{/* علشان اخلى جزء ثابت وجزء متحرك لازم اوت ليت  */}
+<Outlet />
+        <Footer />
       </>
     )
   }
